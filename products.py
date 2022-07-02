f7262ca893b54e4b37a73be154d0432b87dd9210
austin wang <wtwang180829@gamil.com>
2022-07-02 22:58:29 +0800

    basic++

--- products.py
+++ products.py
@@ -7,6 +7,10 @@ while True:
 	price = input('請輸入價錢:')
 	data = [name,price]
 	products.append(data)
-
+	
 for p in products:
 	print (p[0], p[1])
+
+with open('list.txt','w') as f :
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
