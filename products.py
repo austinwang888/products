5204d5cd35ecb9e517010ab6a43bc3a7a73588b5
austin wang <wtwang180829@gamil.com>
2022-07-03 14:09:54 +0800

    add function

--- products.py
+++ products.py
@@ -1,14 +1,19 @@
+import os #operating system
 products = []
 
-with open('list.csv','r',encoding = 'utf-8') as f :
-	for line in f:
-		if '產品,價錢' in line:
-				continue
-		name,price = line.strip().split(',')
-		products.append([name,price])
-
-print(products)
-
+if os.path.isfile('list.csv'):
+	print('yeah,找到檔案')
+	with open('list.csv','r',encoding = 'utf-8') as f :
+		for line in f:
+			if '產品,價錢' in line:
+					continue
+			name,price = line.strip().split(',')
+			products.append([name,price])
+	print(products)
+else:
+	print('找不到檔案...')
+	
+#讓使用者輸入
 while True:
 	name = input ('請輸入商品名稱:')
 	if name == 'q' :
@@ -20,6 +25,7 @@ while True:
 for p in products:
 	print (p[0], p[1])
 
+#寫入檔案
 
 with open('list.csv','w',encoding = 'utf-8') as f :
 	f.write('產品,價錢\n')
