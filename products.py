f48ef08b8c7bf36c1a745bceb4ab149cf434e2ec
austin wang <wtwang180829@gamil.com>
2022-07-03 13:53:45 +0800

    basic++

--- products.py
+++ products.py
@@ -1,5 +1,14 @@
 products = []
 
+with open('list.csv','r',encoding = 'utf-8') as f :
+	for line in f:
+		if '產品,價錢' in line:
+				continue
+		name,price = line.strip().split(',')
+		products.append([name,price])
+
+print(products)
+
 while True:
 	name = input ('請輸入商品名稱:')
 	if name == 'q' :
